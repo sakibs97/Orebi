1564a0c6b7b7c35dec49b74d009b1781f27be4ca
Sakib Hossain <sakibs.ngn@gmail.com>
2024-06-27 14:58:54 +0600

    eight

--- src/pages/Singup.jsx
+++ src/pages/Singup.jsx
@@ -79,44 +79,44 @@ const Singup = () => {
             </div>
             <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
               <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Division</h5>
-              <form className="max-w-sm mx-auto font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]">
-                <select id="countries" className="bg-[#fff] text-gray-900 text-sm rounded-lg block w-full py-[10px]  dark:text-">
-                  <option selected>Please select</option>
-                  <option value="BD-A">Barisal</option>
-                  <option value="BD-B">Chittagong</option>
-                  <option value="BD-C">Dhaka</option>
-                  <option value="BD-D">Khulna</option>
-                  <option value="BD-H">Mymensingh</option>
-                  <option value="BD-E">Rajshahi</option>
-                  <option value="BD-F">Rangpur</option>
-                  <option value="BD-G">Sylhet</option>
+              <form className="max-w-sm mx-auto">
+                <select id="countries" className="bg-[#fff] font-dm font-normal text-[#262626] text-[14px] rounded-lg block w-full py-[10px]">
+                  <option className="font-dm font-normal text-[#262626] text-[14px]" selected>Please select</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]" value="BD-A">Barisal</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]" value="BD-B">Chittagong</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]" value="BD-C">Dhaka</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]" value="BD-D">Khulna</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]" value="BD-H">Mymensingh</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]" value="BD-E">Rajshahi</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]" value="BD-F">Rangpur</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]" value="BD-G">Sylhet</option>
                 </select>
               </form>
             </div>
             <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
               <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">District</h5>
-              <form className="max-w-sm mx-auto font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]">
-                <select id="countries" className="bg-[#fff] text-gray-900 text-sm rounded-lg block w-full py-[10px]  dark:text-">
-                  <option selected>Please select</option>
-                  <option>Dhaka</option>
-                  <option>Faridpur</option>
-                  <option>Gazipur</option>
-                  <option>Gopalganj</option>
-                  <option>Jamalpur</option>
-                  <option>Kishoreganj</option>
-                  <option>Madaripur</option>
-                  <option>Manikganj</option>
-                  <option>Munshiganj</option>
-                  <option>Mymensingh</option>
-                  <option>Narayanganj</option>
-                  <option>Narsingdi</option>
-                  <option>Netrokona</option>
-                  <option>Rajbari</option>
-                  <option>Shariatpur</option>
-                  <option>Sherpur</option>
-                  <option>Tangail</option>
-                  <option>Bogra</option>
-                  <option>Joypurhat</option>
+              <form className="max-w-sm mx-auto text-[red]">
+                <select id="countries" className="bg-[#fff] text-gray-900 text-sm rounded-lg block w-full py-[10px]">
+                  <option selected className="font-dm font-normal text-[#262626] text-[14px]">Please select</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Dhaka</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Faridpur</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Gazipur</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Gopalganj</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Jamalpur</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Kishoreganj</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Madaripur</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Manikganj</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Munshiganj</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Mymensingh</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Narayanganj</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Narsingdi</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Netrokona</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Rajbari</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Shariatpur</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Sherpur</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Tangail</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Bogra</option>
+                  <option className="font-dm font-normal text-[#262626] text-[14px]">Joypurhat</option>
                 </select>
               </form>
             </div>
