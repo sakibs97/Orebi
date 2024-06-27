64ea2690433b05ca2c4002ac4a663f7885aea68c
Sakib Hossain <sakibs.ngn@gmail.com>
2024-06-27 14:47:06 +0600

    sevenn

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,37 +23,37 @@ const Login = () => {
 
 
     return (
-        <Container>
-            <div className="my-[30px]">
-                <h3 className="font-dm font-bold text-[40px] text-[#262626]">Login</h3>
-                <span className="font-dm font-normal text-[15px] text-[#767676]"><Link to="/" className="hover:text-[#262626]">
+        <Container className="px-[10px] lg:px-0">
+            <div className="my-[10px] lg:my-[30px]">
+                <h3 className="font-dm font-bold text-[25px] lg:text-[40px] text-[#262626]">Login</h3>
+                <span className="font-dm font-normal text-[12px] lg:text-[15px] text-[#767676]"><Link to="/" className="hover:text-[#262626]">
                     Home</Link> &gt; Login</span>
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] pb-[30px]">
-                <div className="w-[50%]">
-                    <p className="font-dm font-normal text-[15px] text-[#767676]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.</p>
+            <div className="border-b-[1px] border-[#F0F0F0] pb-[10px] lg:pb-[30px]">
+                <div className="lg:w-[50%]">
+                    <p className="font-dm font-normal text-[12px] lg:text-[15px] text-[#767676]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.</p>
                 </div>
             </div>
             <div className="border-b-[1px] border-[#F0F0F0]">
-                <div className="w-[50%]">
-                    <h4 className="font-dm font-bold text-[35px] text-[#262626] my-[30px]">Returning Customer</h4>
-                    <div className="flex justify-between items-center">
-                        <div className="border-b-[1px] border-[#F0F0F0] w-[45%]">
-                            <h5 className="font-dm font-bold text-[16px] text-[#262626]">Email address</h5>
-                            <input onChange={(e) => setemail(e.target.value)} type="email" placeholder="company@domain.com" className="py-[10px] outline-none w-[100%]" />
+                <div className="lg:w-[50%] ">
+                    <h4 className="font-dm font-bold text-[20px] lg:text-[35px] text-[#262626] my-[15px] lg:my-[30px]">Returning Customer</h4>
+                    <div className="lg:flex justify-between items-center">
+                        <div className="border-b-[1px] border-[#F0F0F0] mb-[10px] lg:mb-0 lg:w-[45%]">
+                            <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626] pb-[5px]">Email address</h5>
+                            <input onChange={(e) => setemail(e.target.value)} type="email" placeholder="company@domain.com" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
                         </div>
-                        <div className="border-b-[1px] border-[#F0F0F0] w-[45%]">
-                            <h5 className="font-dm font-bold text-[16px] text-[#262626]">Password</h5>
-                            <input onChange={(e) => setPassword(e.target.value)} type="Password" placeholder="********" className="py-[10px] outline-none w-full" />
+                        <div className="border-b-[1px] border-[#F0F0F0] lg:w-[45%]">
+                            <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626] pb-[5px]">Password</h5>
+                            <input onChange={(e) => setPassword(e.target.value)} type="Password" placeholder="#######" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
                         </div>
                     </div>
-                    <button onClick={handelLogin} className="my-[30px] font-dm font-bold text-[15px] text-[#262626] border-[1px] border-[#262626] py-[15px] px-[80px] hover:text-[#FFF] hover:bg-[#262626] hover:shadow-xl ease-in-out duration-500">Login</button>
+                    <button onClick={handelLogin} className="my-[30px] font-dm font-bold text-[15px] text-[#262626] border-[1px] border-[#262626] w-full lg:w-[200px] h-[50px] hover:text-[#FFF] hover:bg-[#262626] hover:shadow-xl ease-in-out duration-500">Login</button>
                 </div>
             </div>
-            <div className="w-[50%]">
-                <h4 className="font-dm font-bold text-[35px] text-[#262626] my-[30px]">New Customer</h4>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.</p>
-                <button className="my-[30px] font-dm font-bold text-[15px] text-[#262626] border-[1px] border-[#262626] py-[15px] px-[80px] hover:text-[#FFF] hover:bg-[#262626] hover:shadow-xl ease-in-out duration-500"><Link to="/singup">
+            <div className="w-full lg:w-[50%]">
+                <h4 className="font-dm font-bold text-[20px] lg:text-[35px] text-[#262626] my-[15px] lg:my-[30px]">New Customer</h4>
+                <p className="font-dm font-normal text-[12px] lg:text-[15px] text-[#767676]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.</p>
+                <button className="my-[30px] font-dm font-bold text-[15px] text-[#262626] border-[1px] border-[#262626] w-full lg:w-[200px] h-[50px] hover:text-[#FFF] hover:bg-[#262626] hover:shadow-xl ease-in-out duration-500"><Link to="/singup">
                     Continue</Link></button>
             </div>
         </Container>
--- src/pages/Singup.jsx
+++ src/pages/Singup.jsx
@@ -23,63 +23,63 @@ const Singup = () => {
   }
 
   return (
-    <Container>
-      <div className="my-[30px]">
-        <h3 className="font-dm font-bold text-[40px] text-[#262626]">Sign up</h3>
-        <span className="font-dm font-normal text-[15px] text-[#767676]"><Link to="/" className="hover:text-[#262626]">
+    <Container className="px-[10px] lg:px-0">
+      <div className="my-[10px] lg:my-[30px]">
+        <h3 className="font-dm font-bold text-[25px] lg:text-[40px] text-[#262626]">Sign up</h3>
+        <span className="font-dm font-normal text-[12px] lg:text-[15px] text-[#767676]"><Link to="/" className="hover:text-[#262626]">
           Home</Link> &gt; Sign up</span>
       </div>
-      <div className="border-b-[1px] border-[#F0F0F0] pb-[30px]">
-        <div className="w-[50%]">
-          <p className="font-dm font-normal text-[15px] text-[#767676]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.</p>
+      <div className="border-b-[1px] border-[#F0F0F0] pb-[10px] lg:pb-[30px]">
+        <div className="w-full lg:w-[50%]">
+          <p className="font-dm font-normal text-[12px] lg:text-[15px] text-[#767676]">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the.</p>
         </div>
       </div>
-      <div className="border-b-[1px] border-[#F0F0F0] pb-[30px] mb-[30px]">
-        <div className="w-[50%]">
-          <h4 className="font-dm font-bold text-[35px] text-[#262626] my-[30px]">Your Personal Details</h4>
+      <div className="lg:border-b-[1px] border-[#F0F0F0] lg:pb-[30px] lg:mb-[30px]">
+        <div className="w-full lg:w-[50%]">
+          <h4 className="font-dm font-bold text-[20px] lg:text-[35px] text-[#262626] my-[15px] lg:my-[30px]">Your Personal Details</h4>
           <div className="flex justify-between flex-wrap items-center">
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">First Name</h5>
-              <input onChange={(e) => setFirstName(e.target.value)} type="text" placeholder="First Name" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">First Name</h5>
+              <input onChange={(e) => setFirstName(e.target.value)} type="text" placeholder="First Name" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">Last Name</h5>
-              <input onChange={(e) => setLastName(e.target.value)} type="text" placeholder="Last Name" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Last Name</h5>
+              <input onChange={(e) => setLastName(e.target.value)} type="text" placeholder="Last Name" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">Email address</h5>
-              <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="company@domain.com" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Email address</h5>
+              <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="company@domain.com" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">Telephone</h5>
-              <input onChange={(e) => setPassword(e.target.value)} type="tel" placeholder="Your phone number" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Telephone</h5>
+              <input onChange={(e) => setPassword(e.target.value)} type="tel" placeholder="Your phone number" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
           </div>
         </div>
       </div>
-      <div className="border-b-[1px] border-[#F0F0F0] pb-[30px] mb-[30px]">
-        <div className="w-[50%]">
-          <h4 className="font-dm font-bold text-[35px] text-[#262626] my-[30px]">New Customer</h4>
+      <div className="lg:border-b-[1px] border-[#F0F0F0] lg:pb-[30px] lg:mb-[30px]">
+        <div className="w-full lg:w-[50%]">
+          <h4 className="font-dm font-bold text-[17px] lg:text-[35px] text-[#262626] my-[15px] lg:my-[30px]">New Customer</h4>
           <div className="flex justify-between flex-wrap items-center">
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">Address 1</h5>
-              <input type="text" placeholder="4279 Zboncak Port Suite 6212" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Address 1</h5>
+              <input type="text" placeholder="4279 Zboncak Port Suite 6212" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">Address 2</h5>
-              <input type="text" placeholder="_ _" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Address 2</h5>
+              <input type="text" placeholder="_ _" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">City</h5>
-              <input type="text" placeholder="Your city" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">City</h5>
+              <input type="text" placeholder="Your city" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">Post Code</h5>
-              <input type="number" placeholder="05228" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Post Code</h5>
+              <input type="number" placeholder="05228" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">Division</h5>
-              <form className="max-w-sm mx-auto">
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Division</h5>
+              <form className="max-w-sm mx-auto font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]">
                 <select id="countries" className="bg-[#fff] text-gray-900 text-sm rounded-lg block w-full py-[10px]  dark:text-">
                   <option selected>Please select</option>
                   <option value="BD-A">Barisal</option>
@@ -93,9 +93,9 @@ const Singup = () => {
                 </select>
               </form>
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">District</h5>
-              <form className="max-w-sm mx-auto">
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">District</h5>
+              <form className="max-w-sm mx-auto font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]">
                 <select id="countries" className="bg-[#fff] text-gray-900 text-sm rounded-lg block w-full py-[10px]  dark:text-">
                   <option selected>Please select</option>
                   <option>Dhaka</option>
@@ -123,17 +123,17 @@ const Singup = () => {
           </div>
         </div>
       </div>
-      <div className="border-b-[1px] border-[#F0F0F0] pb-[30px] mb-[30px]">
-        <div className="w-[50%]">
-          <h4 className="font-dm font-bold text-[35px] text-[#262626] my-[30px]">Your Password</h4>
+      <div className="lg:border-b-[1px] border-[#F0F0F0] pb-[30px] lg:mb-[30px]">
+        <div className="w-full lg:w-[50%]">
+          <h4 className="font-dm font-bold text-[17px] lg:text-[35px] text-[#262626] my-[15px] lg:my-[30px]">Your Password</h4>
           <div onChange={(e) => setPassword(e.target.value)} className="flex justify-between flex-wrap items-center">
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">Password</h5>
-              <input type="password" placeholder="Password" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Password</h5>
+              <input type="password" placeholder="Password" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
-            <div className="border-b-[1px] border-[#F0F0F0] w-[45%] my-[10px]">
-              <h5 className="font-dm font-bold text-[16px] text-[#262626]">Repeat Password</h5>
-              <input type="password" placeholder="Repeat Password" className="py-[10px] outline-none w-[100%]" />
+            <div className="border-b-[1px] border-[#F0F0F0] w-full lg:w-[45%] my-[5px] lg:my-[10px]">
+              <h5 className="font-dm font-bold text-[14px] lg:text-[16px] text-[#262626]">Repeat Password</h5>
+              <input type="password" placeholder="Repeat Password" className="py-[5px] outline-none w-full placeholder:font-dm placeholder:font-normal placeholder:text-[12px] placeholder:text-[#767676] font-dm font-normal text-[14px] lg:text-[16px] text-[#262626]" />
             </div>
           </div>
         </div>
@@ -143,7 +143,7 @@ const Singup = () => {
           <input type="checkbox" />
           <p className="font-dm font-normal text-[14px] text-[#767676] pl-[10px]">I have read and agree to the Privacy Policy</p>
         </div>
-        <div className="flex items-center justify-between w-[20%] py-[20px]">
+        <div className="flex items-center justify-between w-[70%] lg:w-[20%] py-[20px]">
           <p className="font-dm font-normal text-[14px] text-[#767676]">Subscribe Newsletter</p>
           <div className="">
             <input id="default-checkbox" type="checkbox" />
@@ -155,7 +155,7 @@ const Singup = () => {
           </div>
         </div>
       </div>
-      <button onClick={handelSingup} className="mb-[30px] font-dm font-bold text-[15px] text-[#262626] border-[1px] border-[#262626] py-[15px] px-[80px] hover:text-[#FFF] hover:bg-[#262626] hover:shadow-xl ease-in-out duration-500">Login</button>
+      <button onClick={handelSingup} className="mb-[30px] font-dm font-bold text-[15px] text-[#262626] border-[1px] border-[#262626] py-[15px] w-full lg:w-[300px] hover:text-[#FFF] hover:bg-[#262626] hover:shadow-xl ease-in-out duration-500">Login</button>
 
     </Container>
   )
